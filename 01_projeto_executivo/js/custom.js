057de85733fc6966324de47891068689d6122fab
ymbitancourt <115833606+ymbitancourt@users.noreply.github.com>
2024-07-15 23:53:20 -0300

    ajuste

---
+++ 01_projeto_executivo/js/custom.js
@@ -0,0 +1,38 @@
+var FXManager = {
+    snd_certo : new Audio("objetos/snd/acerto.mp3"),
+    snd_erro : new Audio("objetos/snd/erro.mp3"),
+    snd_aplauso : new Audio("objetos/snd/aplauso.mp3")
+}
+
+$(".menu label.menu-toggle").click(function(){
+    $(".bg-fundo").toggleClass("mod");
+//    $('body').toggleClass('overflow');
+    $('.menu').toggleClass('fade-check');
+});
+
+$(".bg-fundo").click(function(){
+    $(this).removeClass("mod");
+    $('body').removeClass('overflow');
+    $('.menu').removeClass('fade-check');
+});
+
+$(".fundo-item").click(function(){
+    $(".bg-fundo").removeClass("mod");
+    $('body').removeClass('overflow');
+    $('.menu').removeClass('fade-check');
+});
+
+
+/*
+jQuery("document").ready(function($){
+
+    var pg_number = parseInt(window.location.href.split('?')[1].split('=')[1]);
+  
+    if(pg_number == 1){
+      document.body.style.backgroundImage = "url('objetos/bg01.png')";
+    }else if(pg_number == 2){
+      document.body.style.backgroundImage = "url('objetos/bg02.png')";
+    }
+    
+});
+*/
